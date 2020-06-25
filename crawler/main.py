e012e350d02ee228a9cd85399b4c8b5728d1cd65
Sven Andersson <svena@tutanota.com>
2020-06-25 11:18:34 +0200

    arguments and cleanup
    - new min wait time arguement
    - tidy
    - "better" arument names

--- crawler/main.py
+++ crawler/main.py
@@ -2,13 +2,14 @@
 Scrape kolonial.no
 
 usage:
-    scrape [--concurrent=N] [--site=SITE] [--random-wait=S] [--max-items=N]
+    scrape [--concurrent=N] [--site=URL] [--minimum-wait=S] [--maximum-wait=S] [--max-items=N]
 
 options:
-    --concurrent=N, -C    number of concurrent requests [default: 1]
-    --site=SITE, -S       url for site to scrape [default: https://kolonial.no]
-    --random-wait=S,-R    max seconds to wait before next request [default: 1.0]
-    --max-items=N, -n     max number of items to fetch [default: 100]
+    --concurrent=N, -c        number of concurrent requests [default: 1]
+    --site=SITE, -s           url for site to scrape [default: https://kolonial.no]
+    --minimum-wait=S, -i      minum wait time between requests [default: 1.0]
+    --maximum-wait=S, -a      max seconds to wait before next request [default: 2.0]
+    --max-items=N, -n         max number of items to fetch [default: 100]
 """
 
 
@@ -27,61 +28,26 @@ USER_AGENT = "testcrawler / 0.1 an execercise"
 Response = namedtuple("Response", ["status_code", "text"])
 
 
-class HttpClient:
-    def __init__(self, max_wait):
-        self.session = aiohttp.ClientSession(
-            headers={"User-Agent": USER_AGENT}, raise_for_status=True
-        )
-        self.max_wait = max_wait
-        self.gets = 0
-
-    async def get(self, url):
-        await asyncio.sleep(random.uniform(0.0, self.max_wait))
-        self.gets += 1
-        response = await self.session.get(url)
-        async with response:
-            return Response(status_code=response.status, text=await response.text())
-
-    async def close(self):
-        await self.session.close()
-
-
-class Emitter:
-    def __init__(self, max_items):
-        self.count = 0
-        self.max_items = max_items
-
-    def emit(self, product):
-        self.count += 1
-        print(product)
-
-    def enough(self):
-        return self.count >= self.max_items
-
-
 def main():
     args = docopt(__doc__)
 
     concurrent = int(args["--concurrent"])
     site = args["--site"]
-    random_wait = float(args["--random-wait"])
+    minimum_wait = float(args["--minimum-wait"])
+    maximum_wait = float(args["--maximum-wait"])
     max_items = int(args["--max-items"])
 
-    asyncio.run(_main(site, concurrent, random_wait, max_items))
+    asyncio.run(_main(
+        site, concurrent, minimum_wait, maximum_wait, max_items))
 
 
-def create_robot_agent(site):
-    r = Robots.fetch(f"{site}/robots.txt")
-    return r.agent(USER_AGENT)
-
-
-async def _main(site, concurrent, max_wait, max_items):
+async def _main(site, concurrent, minimum_wait, maximum_wait, max_items):
     start_time = time.time()
 
-    robot_agent = create_robot_agent(site)
+    robot_agent = create_robot_agent(USER_AGENT, site)
     emitter = Emitter(max_items)
     handler = KolonialHandlers(site, emitter)
-    http_client = HttpClient(max_wait)
+    http_client = HttpClient(USER_AGENT, minimum_wait, maximum_wait)
     crawler = Crawler(robot_agent, http_client, handler)
 
     queue = asyncio.Queue()
@@ -123,3 +89,43 @@ async def worker_task(n, crawler, queue):
         if crawler.link_handlers.emitter.enough():
             empty_queue(queue)
             return
+
+
+class HttpClient:
+    def __init__(self, user_agent, minimum_wait, maximum_wait):
+        self.session = aiohttp.ClientSession(
+            headers={"User-Agent": user_agent}, raise_for_status=True
+        )
+        self.minimum_wait = minimum_wait
+        self.maximum_wait = maximum_wait 
+        self.gets = 0
+
+    async def get(self, url):
+        await asyncio.sleep(random.uniform(self.minimum_wait, self.maximum_wait))
+        self.gets += 1
+        response = await self.session.get(url)
+        async with response:
+            return Response(status_code=response.status, text=await response.text())
+
+    async def close(self):
+        await self.session.close()
+
+
+class Emitter:
+    def __init__(self, max_items):
+        self.count = 0
+        self.max_items = max_items
+
+    def emit(self, product):
+        self.count += 1
+        print(product)
+
+    def enough(self):
+        return self.count >= self.max_items
+
+
+def create_robot_agent(user_agent, site):
+    r = Robots.fetch(f"{site}/robots.txt")
+    return r.agent(user_agent)
+
+
